9652c1f2489440e0e790d406f8607e6bb2adfc34
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-26 21:13:33 -0300

    será que vai funfar

--- source/Options.hx
+++ source/Options.hx
@@ -1,6 +1,28 @@
 package;
 
+import flixel.FlxG;
+
 class Options
 {
 	public static var masterVolume:Float = 1;
+}
+
+class AndroidControls extends Option
+{
+	public function new()
+	{
+		super();
+	}
+
+	public override function press():Bool
+	{
+		trace("switch");
+		FlxG.switchState(new android.AndroidControlsMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Android Controls";
+	}
 }
